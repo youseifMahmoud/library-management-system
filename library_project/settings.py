78e3d08ff4c0282ab125410502f76ae8405280fb
Yousef Hany Mohamed <61082262+youseifMahmoud@users.noreply.github.com>
2024-09-27 06:32:59 +0300

    Update settings.py

--- library_project/settings.py
+++ library_project/settings.py
@@ -83,7 +83,16 @@ DATABASES = {
     }
 }
 
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'libraryapp',  
+#         'USER': 'yousef',  
+#         'PASSWORD': '4100',  
+#         'HOST': 'localhost',  
+#         'PORT': '5432',  
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
